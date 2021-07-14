8ffd4afb6b0f9354eeea085bab0b685bdbf24d6f
Nithack <andreynithack@gmail.com>
2021-07-14 00:07:46 -0300

    feat: config pg-db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
   controllers: [],
   providers: [],
 })
