3e8c06616d013752fbd844014ea1169cb9c6f917
Nithack <andreynithack@gmail.com>
2021-07-13 23:51:58 -0300

    feat: configure typeorm witch postgres

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    logger.log('Server liste http://localhost:3000');
+  });
 }
 bootstrap();
